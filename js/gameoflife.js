588ef6c9204d0fe4c96533a2ef89f1ab5dc01aa9
Isabel TS <izzytons@gmail.com>
2021-05-19 12:10:04 -0400

    fix printCell

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -13,6 +13,26 @@ function contains(cell) {
 }
 
 const printCell = (cell, state) => {
+  return contains.call(state, cell) ? "\u25A3" : "\u25A2";
+};
+
+const corners = (state = []) => {
+  if (state.length === 0) {
+    return {
+      topRight: [0, 0],
+      bottomLeft: [0, 0]
+    };
+  }
+
+  const xs = state.map(([x, _]) => x);
+  const ys = state.map(([_, y]) => y);
+  return {
+    topRight: [Math.max(...xs), Math.max(...ys)],
+    bottomLeft: [Math.min(...xs), Math.min(...ys)]
+  };
+};
+
+const printCells = (state) => {
   const { bottomLeft, topRight } = corners(state);
   let accumulator = "";
   for (let y = topRight[1]; y >= bottomLeft[1]; y--) {
@@ -23,35 +43,6 @@ const printCell = (cell, state) => {
     accumulator += row.join(" ") + "\n";
   }
   return accumulator;
-};
-
-const corners = (state = []) => {
-
-  if (state.length === 0) {
-    return {
-      topRight: [0, 0],
-      bottomLeft: [0, 0]
-    };
-  }
-
-  const xs = state.map((x, _]) => x);
-const ys = state.map((_, y]) => y);
-
-return {
-  topRight: [Math.max(...xs), Math.max(...ys)],
-  bottomLeft: [Math.min(...xs), Math.min(...ys)]
-};
-};
-
-const printCells = (state) => {
-  console.log(this, state);
-  let bottom_row = parseInt(corners(state).bottomLeft[1]);
-  let top_row = parseInt(corners(state).topRight[1]);
-  for (var i = bottom_row, i < parseInt(top_row), i++) {
-
-  }
-  printCell(corners(state).bottomLeft);
-
 
 };
 
