a9e2fd24277ef62fe6c1faf756ba73a1e20152e7
Isabel TS <izzytons@gmail.com>
2021-05-19 11:16:55 -0400

    complete contains function

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -9,8 +9,8 @@ function same([x, y], [j, k]) {
 
 // The game state to search for `cell` is passed as the `this` value of the function.
 function contains(cell) {
-  
- }
+  return this.includes(cell);
+}
 
 const printCell = (cell, state) => { };
 
